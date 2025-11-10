c897be3686622ff8de4beef40741874dd5532974
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-11-10 11:25:13 -0500

    Create file and add starting plans and notes

---
+++ script.js
@@ -0,0 +1,13 @@
+// Linked list
+
+// Files will need to be transferred to modules for testing. Revisit ODIN page on modules: https://www.theodinproject.com/lessons/javascript-es6-modules
+
+//Linked lists src guide: https://web.archive.org/web/20200217010131/http://www.cs.cmu.edu/~adamchik/15-121/lectures/Linked%20Lists/linked%20lists.html
+
+// Starting notes:
+// You will need an inner and outer class -> The outer LinkedList class will have an inner Node class
+// The purose of the inner class is to be an internal helper to the outer class
+// Node class will contain a value property and a nextNode property - both set as null by default
+
+// Revisit MDN docs on classes and associated properties (static, public, private etc.)
+// Odin src on classes: https://www.theodinproject.com/lessons/node-path-javascript-classes
