bad03db6664fe136eab6db9fc25169bd3af3fde9
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-11-12 15:43:02 -0500

    Rewrite pop() method for clarity, remove notes, and add/remove console.logs for additional clarity

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 // Linked list
 
-// Files will need to be transferred to modules for testing. Revisit ODIN page on modules: https://www.theodinproject.com/lessons/javascript-es6-modules
-
-//Linked lists src guide: https://web.archive.org/web/20200217010131/http://www.cs.cmu.edu/~adamchik/15-121/lectures/Linked%20Lists/linked%20lists.html
-
-// Starting notes:
-// You will need an inner and outer class -> The outer LinkedList class will have an inner Node class
-// The purose of the inner class is to be an internal helper to the outer class
-// Node class will contain a value property and a nextNode property - both set as null by default
-
-// Revisit MDN docs on classes and associated properties (static, public, private etc.)
-// Odin src on classes: https://www.theodinproject.com/lessons/node-path-javascript-classes
-
-// To test, we're creating an instance of LinkedList:
-// list.append("dog")
-// methods are on the LinkedList class
-
-// https://www.geeksforgeeks.org/javascript/implementation-linkedlist-javascript/
-
 class LinkedList {
   constructor() {
     this.head = null;
@@ -29,10 +11,8 @@ class LinkedList {
   }
 
   append(value) {
-    // add value to the end of the list
     if (this.head == null) {
       this.head = new Node(value);
-      console.log(this.head);
     } else {
       let tmp = this.head;
       while (tmp.nextNode != null) {
@@ -50,9 +30,9 @@ class LinkedList {
     if (this.head == null) {
       this.printError_ListEmpty();
     } else {
-      let sum = 1;
+      let sum = 0;
       let tmp = this.head;
-      while (tmp.nextNode != null) {
+      while (tmp != null) {
         tmp = tmp.nextNode;
         sum++;
       }
@@ -95,15 +75,17 @@ class LinkedList {
   }
 
   pop() {
-    // remove element from the end of the list
     if (this.head == null) {
       this.printError_ListEmpty();
     } else {
-      let tmp = this.head;
-      while (tmp.nextNode.nextNode != null) {
-        tmp = tmp.nextNode;
+      let prev = null;
+      let current = this.head;
+      while (current.nextNode != null) {
+        prev = current;
+        current = current.nextNode;
       }
-      tmp.nextNode = null;
+      console.log(`Removed ${current.value}`);
+      prev.nextNode = null;
     }
   }
 
@@ -164,7 +146,6 @@ class LinkedList {
 
   insertAt(value, index) {
     if (this.head == null) {
-      // if list is empty, add value at the head
       this.head = new Node(value);
     } else {
       let counter = 0;
@@ -175,13 +156,13 @@ class LinkedList {
         current = current.nextNode;
         counter++;
       }
-      // if index exceeds the length of the list, just place the value at end of list
       if (current == null) {
-        console.log("index exceeds counter");
-        console.log(current);
-        // if current is null, you can't simply replace null with the new node, you have to point the previous node to the new node, like below
+        console.log(
+          `Linked list is shorter than ${index}, so ${value} was placed at the end of the list instead`
+        );
         prev.nextNode = new Node(value);
       } else {
+        console.log(`${value} was inserted at ${counter}`);
         prev.nextNode = new Node(value, current);
       }
     }
@@ -200,9 +181,11 @@ class LinkedList {
         counter++;
       }
       if (current == null) {
-        console.log(`There is no value at index: ${index}`);
+        console.log(
+          `There is no value at index: ${index}, so nothing was removed`
+        );
       } else {
-        console.log(current);
+        console.log(`Removed ${current.value} at index: ${index}`);
         prev.nextNode = current.nextNode;
       }
     }
@@ -216,42 +199,4 @@ class Node {
   }
 }
 
-// example uses class syntax - adjust as necessary
-const list = new LinkedList();
-
-list.append("dog");
-list.append("cat");
-list.append("parrot");
-list.append("hamster");
-list.append("snake");
-list.append("turtle");
-
-// Tests
-// list.prepend("Put me first!");
-console.log(`# of values in the list: ${list.size()}`);
-console.log(list.header());
-console.log(list.tail());
-console.log(list.at(0));
-console.log(list.at(1));
-console.log(list.at(2));
-console.log(list.pop());
-console.log(list.contains("dog"));
-console.log(list.contains("fish"));
-console.log(list.find("Put me first!"));
-console.log(list.find("parrot"));
-console.log(list.find("fish"));
-console.log(list.toString());
-
-// console.log(list);
-
-list.insertAt("fish", 2);
-console.log(list.toString());
-
-list.insertAt("horse", 12);
-console.log(list.toString());
-
-list.removeAt(4);
-console.log(list.toString());
-
-list.removeAt(12);
-console.log(list.toString());
+export { LinkedList };
